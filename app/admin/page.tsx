1996fb68b92f2b7500ae12112a57ff1c2c6d4b56
khemu <alghost2004@gmail.com>
2024-09-18 14:52:57 +0300

    .

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export const runtime = "edge";
 
 const Admin = () => {
-  return <div>page</div>;
+  return <div className="m-auto text-4xl">WIP</div>;
 };
 
 export default Admin;
